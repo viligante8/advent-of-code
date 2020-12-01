f61f8376069a6c296e3e1a468cedfb2cbff3ddac
Vito Pistelli <vito.pistelli@razartech.com>
2020-12-01 16:29:00 -0500

    better performance by removing nested loop

--- 2020/1/day1.2.js
+++ 2020/1/day1.2.js
@@ -1,18 +1,20 @@
 const fs = require('fs');
 
-const input = fs.readFileSync('./input').toString().split('\n').map(i => parseInt(i));
+let input = fs.readFileSync('./input').toString().split('\n').map(i => parseInt(i));
 
-let sums = {};
+let result = {};
 
-for (let i = 0; i < input.length; i++) {
-  for (let j = 1; j < input.length; j++) {
-    for (let k = 2; k < input.length; k++) {
-      sums[input[i] + input[j] + input[k]] = [input[i], input[j], input[k]];
-      if (input[i] + input[j] + input[k] === 2020) {
-        console.log(`${input[i]} + ${input[j]} + ${input[k]} = ${input[i] + input[j] + input[k]}`);
-      }
-    }
+for(let i = 0; i < input.length; i++) {
+  for(let j = i + 1; j < input.length; j++) {
+    const diff = 2020 - (input[i] + input[j]);
+    if(diff > 0)
+      result[diff] = [input[i], input[j]];
   }
 }
 
-console.log(sums[2020][0] * sums[2020][1] * sums[2020][2]);
+for (const [key, value] of Object.entries(result)) {
+  if(result[value[0]] && result[value[1]]) {
+    console.log(key * value[0] * value[1]);
+    break;
+  }
+}
