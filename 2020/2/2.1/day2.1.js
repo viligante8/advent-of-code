36ff4e11180e617a8587023a5f57edccf89887a2
Vito Pistelli <vito.pistelli@razartech.com>
2020-12-02 09:18:07 -0500

    day 2 first attempts

---
+++ 2020/2/2.1/day2.1.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+// Sample Input
+// 1-3 a: abcde
+// 1-3 b: cdefg
+// 2-9 c: ccccccccc
+
+const checkPasswords = () => {
+  const input = fs.readFileSync('../input').toString().split('\n');
+  let correctCount = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    const policyCountLowerBound = input[i].split(':')[0].split(' ')[0].split('-')[0];
+    const policyCountUpperBound = input[i].split(':')[0].split(' ')[0].split('-')[1];
+    const policyLetter = input[i].split(':')[0].split(' ')[1];
+    const password = input[i].split(':')[1];
+    const passwordArr = password.split('');
+    passwordArr.shift();
+
+    const letterCounts = passwordArr.reduce((acc, curr) => {
+      if (acc[curr]) {
+        acc[curr]++;
+      } else {
+        acc[curr] = 1;
+      }
+      return acc;
+    }, {});
+
+    console.log(letterCounts);
+    if (letterCounts[policyLetter] >= policyCountLowerBound && letterCounts[policyLetter] <= policyCountUpperBound){
+      correctCount++;
+    }
+  }
+
+  return correctCount;
+};
+
+console.log(checkPasswords());
+
+module.exports.checkPasswords;
---
+++ 2020/2/2.2/day2.2.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+// Sample Input
+// 1-3 a: abcde
+// 1-3 b: cdefg
+// 2-9 c: ccccccccc
+
+const checkPasswords = () => {
+  const input = fs.readFileSync('../input').toString().split('\n');
+  let correctCount = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    const policyCountIndex1 = input[i].split(':')[0].split(' ')[0].split('-')[0]-1;
+    const policyCountIndex2 = input[i].split(':')[0].split(' ')[0].split('-')[1]-1;
+    const policyLetter = input[i].split(':')[0].split(' ')[1];
+    const password = input[i].split(':')[1];
+    const passwordArr = password.split('');
+    passwordArr.shift();
+
+    if(passwordArr[policyCountIndex1] === policyLetter ^ passwordArr[policyCountIndex2] === policyLetter) {
+      correctCount++;
+    }
+  }
+
+  return correctCount;
+};
+
+console.log(checkPasswords());
+
+module.exports.checkPasswords;
